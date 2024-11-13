7c1d3a31d6c51203dff05a5f827a4195ce2d0feb
David Alsh <alshdavid@gmail.com>
2024-11-13 12:44:13 +0700

    Renamed and ci

---
+++ crates/deno_v8_snapshot/README.md
@@ -0,0 +1,3 @@
+# Deno V8 Snapshot (Unofficial)
+
+This crate provides prebuilt Deno V8 Snapshots for use when embedding Deno into Rust applications
--- crates/deno_v8_snapshot/src/lib.rs
+++ crates/deno_v8_snapshot/src/lib.rs
@@ -1,2 +1,2 @@
 #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-pub use deno_embed_macos_arm64::V8_SNAPSHOT;
+pub use deno_v8_snapshot_macos_arm64::V8_SNAPSHOT;
